96dc015d7aa032d08efb5412585188b5edc76ab0
davidv <davidvazquezgit@gmail.com>
2023-03-15 11:27:55 -0700

    work on image list componnent

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import searchImages from "./api";
 import ImageList from "./components/ImageList";
 import SearchBar from "./components/SearchBar";
 
 function App() {
-  const handleSubmit = (term) => {
-    searchImages(term)
+  const [images, setImages] = useState([])
+
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term)
+    setImages(result)
   }
 
   return (
     <>
       <SearchBar onSubmit={handleSubmit}/>
-      <ImageList />
+      <ImageList images={images}/>
     </>
   );
 }
