0556bc1420f23ae8b51425fae7a68ee04787acad
davidv <davidvazquezgit@gmail.com>
2023-03-16 11:59:37 -0700

    add some styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './SearchBar.css'
 
 function SearchBar({onSubmit}) {
   const [term, setTerm] = useState('')
@@ -13,8 +14,9 @@ function SearchBar({onSubmit}) {
   }
 
   return (
-    <div>
+    <div className="search-bar">
       <form onSubmit={handleFormSubmit}>
+        <label htmlFor="">Enter Search Term</label>
         <input value={term} onChange={handleChange} type="text" />
       </form>
     </div>
